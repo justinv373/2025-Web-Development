87b999178d44484ea0709b8dd7f3031905130a60
justinv373 <justinv373@nycstudents.net>
2025-12-22 14:22:31 +0000

    Update practical 3.3

--- WDUnit3/WDLesson3_3Practical/script.js
+++ WDUnit3/WDLesson3_3Practical/script.js
@@ -1,1 +1,12 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+<script>
+    function knick1(){
+        document.getElementById("highlight").innerHTML
+    }
+
+
+
+
+
+
+</script>
