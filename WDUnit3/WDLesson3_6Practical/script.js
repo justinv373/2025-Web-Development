e50c0ba3f4083858f5059f95ea4000d586410b23
justinv373 <justinv373@nycstudents.net>
2026-01-16 14:32:48 +0000

    Update practical 3.6

--- WDUnit3/WDLesson3_6Practical/script.js
+++ WDUnit3/WDLesson3_6Practical/script.js
@@ -1,7 +1,32 @@
 // Challenge 2:  Create the function for the Average program.  Refer to the resource link in the HTML for assistance.
+function average(){
+    let g1 = parseFloat(document.getElementById("g1").value);
+    let g2 = parseFloat(document.getElementById("g2").value);
+    let g3 = parseFloat(document.getElementById("g3").value);
+    let output = document.getElementById("output_average");
 
+    let avg = (g1 + g2 + g3)/3;
+    output.innerHTML = "Average is " + avg;
+}
 
 // Challenge 4:  Create the function for the Slope program.  Refer to the resource link in the HTML for assistance.
+function slope(){
+    let x1 = parseFloat(document.getElementById("x1").value);
+    let y1 = parseFloat(document.getElementById("y1").value);
+    let x2 = parseFloat(document.getElementById("x2").value);
+    let y2 = parseFloat(document.getElementById("y2").value);
+    let output = document.getElementById("output_slope");
 
+    let slope = (y2 - y1)/(x2 - x1);
+    output.innerHTML = "Slope is " + slope;
+}
 
 // Challenge 6:  Create the function for the BMI program.  Refer to the resource link in the HTML for assistance.
+function bmi(){
+    let h = parseFloat(document.getElementById("height").value);
+    let w = parseFloat(document.getElementById("weight").value);
+    let output = document.getElementById("output_bmi");
+
+    let bmi = (w / (h*h)) * 703;
+    output.innerHTML = "BMI is " + bmi;
+}
